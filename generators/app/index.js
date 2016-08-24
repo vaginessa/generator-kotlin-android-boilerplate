eab9312a5141652af397618e251aaee8c13e0405
Ravindra Kumar <ravidsrk@gmail.com>
2016-08-24 16:39:54 +0530

    Added validations to app name and package

--- generators/app/index.js
+++ generators/app/index.js
@@ -18,11 +18,24 @@ module.exports = yeoman.Base.extend({
       {
         name: 'name',
         message: 'What are you calling your app?',
+        validate: function (input) {
+          if (/^([a-zA-Z0-9_]*)$/.test(input)) {
+            return true;
+          }
+          return 'Your application name cannot contain special characters or a blank space, using the default name instead : ' + this.appname;
+        },
         default: this.appname // Default to current folder name
       },
       {
         name: 'package',
-        message: 'What package will you be publishing the app under?'
+        message: 'What package will you be publishing the app under?',
+        validate: function (input) {
+          if (/^([a-z_]{1}[a-z0-9_]*(\.[a-z_]{1}[a-z0-9_]*)*)$/.test(input)) {
+            return true;
+          }
+          return 'The package name you have provided is not a valid Java package name.';
+        },
+        default: 'in.boilerplate.sample'
       },
       {
         name: 'targetSdk',
